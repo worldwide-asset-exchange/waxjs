4bc4667faaf9546a1e57ebe7cd12e3b755a5e5a9
Juan Pablo Islas <juanpablo.islas@opskins.com>
2019-09-27 14:43:15 -0400

    [KEW-1584] Signing transactions

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,57 @@
 export class WaxJS {
-  constructor(
-    private waxSigningURL: string = 'http://localhost:3000'
-  ) {}
+  constructor(private waxSigningURL: string = "http://localhost:3000") {}
 
   public async login() {
     let confirmationWindow = await this.openTab(this.waxSigningURL + "/login/");
 
     return new Promise((resolve, reject) => {
       const receiveMessage = async (event: any) => {
-
         // validate origin with the ui uri
-        if (event.origin ==! this.waxSigningURL) {
+        if (event.origin !== this.waxSigningURL) {
           reject(new Error("Invalid origin"));
         }
 
-        let {verified, userAccount} = event.data;
+        let { verified, userAccount } = event.data;
         if (verified && userAccount !== null) {
           resolve(userAccount);
         } else {
-          reject(new Error('User denied to share userAccount'))
+          reject(new Error("User denied to share userAccount"));
+        }
+      };
+
+      (window as Window).addEventListener("message", receiveMessage, false);
+    });
+  }
+
+  public async signing(transaction: any) {
+    let confirmationWindow = await this.openTab(
+      this.waxSigningURL + "/signing/"
+    );
+
+    return new Promise((resolve, reject) => {
+      const receiveMessage = async (event: any) => {
+        // validate origin with the ui uri
+        if (event.origin !== this.waxSigningURL) {
+          reject(new Error("Invalid origin"));
+        }
+
+        switch (event.data.type) {
+          case "READY":
+            confirmationWindow.postMessage(
+              { type: "TRANSACTION", transaction },
+              this.waxSigningURL
+            );
+            break;
+          case "TX_SIGNED":
+            let { verified, signature } = event.data;
+            if (verified && signature !== null) {
+              resolve(signature);
+            } else {
+              reject(new Error("User denied to sign transaction"));
+            }
+            break;
+          default:
+            reject(new Error("Invalid data"));
         }
       };
 
