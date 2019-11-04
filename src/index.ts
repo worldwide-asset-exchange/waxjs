362d4a1de7b9f798b18515645d6db6ec8ab999a4
Lucas Garcia <lucas.garcia@opskins.com>
2019-11-04 17:07:00 -0300

    [KEW-1688] Integrate eosjs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import { WaxEventSource } from "./WaxEventSource";
+import { Api, JsonRpc, RpcError } from "eosjs";
 
 export class WaxJS {
   private waxEventSource: WaxEventSource;
+  private readonly rpc: JsonRpc;
+  private readonly api: Api;
+  private pubKeys: string[];
 
-  constructor(private waxSigningURL: string = "http://localhost:3000") {
+  constructor(
+    private waxSigningURL: string = "http://localhost:3000",
+    rcpEndpoint: string
+  ) {
     this.waxEventSource = new WaxEventSource(waxSigningURL);
+    this.rpc = new JsonRpc(rcpEndpoint);
+
+    const signer = {
+      getAvailableKeys: () => {
+        return this.pubKeys;
+      },
+      sign: async (data: any) => {
+        return {
+          serializedTransaction: data.serializedTransaction,
+          signatures: await this.signing(data.serializedTransaction)
+        };
+      }
+    };
+
+    // @ts-ignore
+    this.api = new Api({ rpc: this.rpc, signatureProvider: signer });
   }
 
   public async login() {
@@ -13,12 +36,17 @@ export class WaxJS {
     );
 
     const receiveMessage = async (event: any) => {
-      let { verified, userAccount } = event.data;
-      if (!verified || userAccount == null) {
+      let { verified, userAccount, pubKeys } = event.data;
+      if (!verified) {
         throw new Error("User denied to share userAccount");
       }
 
-      return userAccount;
+      if (userAccount == null || pubKeys == null) {
+        throw new Error("User don't have a blockchain account");
+      }
+
+      this.pubKeys = pubKeys;
+      return { userAccount, rpc: this.rpc, api: this.api };
     };
 
     return this.waxEventSource.onceEvent(
@@ -28,7 +56,7 @@ export class WaxJS {
     );
   }
 
-  public async signing(transaction: any) {
+  private async signing(transaction: any) {
     let confirmationWindow: Window = await this.waxEventSource.openEventSource(
       this.waxSigningURL + "/signing/",
       { type: "TRANSACTION", transaction }
