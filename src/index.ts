6dee54e0ec348ebd1de74ba4d13445cfc1e71a56
Aaron Caswell <aaron@captureplay.com>
2019-11-29 16:24:31 -0800

    [KEW-1776] whitelisted actions + leverage auto sign endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Api, JsonRpc, RpcError } from "eosjs";
 import { WaxEventSource } from "./WaxEventSource";
+import { WhitelistedContract } from "./WhitelistedContract";
 
 export class WaxJS {
   private waxEventSource: WaxEventSource;
@@ -8,10 +9,12 @@ export class WaxJS {
   private userAccount: string;
   private pubKeys: string[];
   private signingWindow: Window;
+  private whitelistedContracts: WhitelistedContract[];
 
   constructor(
     rcpEndpoint: string,
-    private waxSigningURL: string = "https://all-access.wax.io"
+    private waxSigningURL: string = "https://all-access.wax.io",
+    private waxAutoSigningURL: string = "https://api-idm.wax.io/v1/accounts/auto-accept/signing"
   ) {
     this.waxEventSource = new WaxEventSource(waxSigningURL);
     this.rpc = new JsonRpc(rcpEndpoint);
@@ -23,7 +26,12 @@ export class WaxJS {
     );
 
     const receiveMessage = async (event: any) => {
-      const { verified, userAccount, pubKeys } = event.data;
+      const {
+        verified,
+        userAccount,
+        pubKeys,
+        whitelistedContracts
+      } = event.data;
       if (!verified) {
         throw new Error("User declined to share their user account");
       }
@@ -34,6 +42,7 @@ export class WaxJS {
 
       this.userAccount = userAccount;
       this.pubKeys = pubKeys;
+      this.whitelistedContracts = whitelistedContracts;
 
       const signer = {
         getAvailableKeys: () => {
@@ -42,10 +51,7 @@ export class WaxJS {
         sign: async (data: any) => {
           return {
             serializedTransaction: data.serializedTransaction,
-            signatures: await this.signing(
-              this.signingWindow,
-              data.serializedTransaction
-            )
+            signatures: await this.signing(data.serializedTransaction)
           };
         }
       };
@@ -58,9 +64,11 @@ export class WaxJS {
       // By pre-creating the pop-up window we will interact with,
       // we ensure that it is not going to be rejected due to a delayed
       // pop up that would otherwise occur post transaction creation
-      this.api.transact = async (...args) => {
-        this.signingWindow = await window.open(url, "_blank");
-        return await transact(...args);
+      this.api.transact = async (transaction, namedParams) => {
+        if (!this.autoAccept(transaction)) {
+          this.signingWindow = await window.open(url, "_blank");
+        }
+        return await transact(transaction, namedParams);
       };
 
       return this.userAccount;
@@ -73,7 +81,62 @@ export class WaxJS {
     );
   }
 
-  private async signing(window: Window, transaction: any) {
+  private autoAccept(transaction: any) {
+    const deserializedTransaction = transaction.actions
+      ? transaction
+      : this.api.deserializeTransaction(transaction);
+    return (
+      undefined !==
+      deserializedTransaction.actions.find((a: any) => {
+        return this.whitelistedContracts.find(w => w.contract === a.account);
+      })
+    );
+  }
+
+  private async signing(transaction: any) {
+    if (this.autoAccept(transaction)) {
+      return this.signViaEndpoint(transaction);
+    }
+
+    return this.signViaWindow(this.signingWindow, transaction);
+  }
+
+  private async signViaEndpoint(transaction: any) {
+    try {
+      let json;
+      let response;
+      try {
+        const f = (global as any).fetch;
+        response = await f(this.waxAutoSigningURL, {
+          body: JSON.stringify({
+            serializedTransaction: Object.values(transaction)
+          }),
+          method: "POST"
+        });
+        json = await response.json();
+        if (json.processed && json.processed.except) {
+          throw new Error(json);
+        }
+      } catch (e) {
+        e.isFetchError = true;
+        throw e;
+      }
+      if (!response.ok) {
+        throw new Error(json);
+      }
+      const { verified, signatures } = json;
+      if (!verified || signatures == null) {
+        throw new Error("Endpoint failed to sign the transaction");
+      }
+      return signatures;
+    } catch (e) {
+      // clear the whitelist to make sure we don't repeatedly attempt blocked actions
+      this.whitelistedContracts = [];
+      throw e;
+    }
+  }
+
+  private async signViaWindow(window: Window, transaction: any) {
     const confirmationWindow: Window = await this.waxEventSource.openEventSource(
       this.waxSigningURL + "/cloud-wallet/signing/",
       { type: "TRANSACTION", transaction },
@@ -82,7 +145,8 @@ export class WaxJS {
 
     const signTransaction = async (event: any) => {
       if (event.data.type === "TX_SIGNED") {
-        const { verified, signatures } = event.data;
+        const { verified, signatures, whitelistedContracts } = event.data;
+        this.whitelistedContracts = whitelistedContracts;
         if (!verified || signatures == null) {
           throw new Error("User declined to sign the transaction");
         }
