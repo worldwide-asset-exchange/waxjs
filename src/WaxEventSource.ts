e13401e972ffae8fc9a8eff31d7b983aac7180f0
Fabian Emilius <fabian.emilius@tum.de>
2021-09-04 02:27:26 +0200

    Format code

--- src/WaxEventSource.ts
+++ src/WaxEventSource.ts
@@ -1,82 +1,82 @@
 export class WaxEventSource {
-  constructor(private waxSigningURL: string) {
-    this.openEventSource = this.openEventSource.bind(this);
-    this.onceEvent = this.onceEvent.bind(this);
-  }
-
-  public async openPopup(url: string): Promise<Window> {
-    const win = await window.open(url, 'WaxPopup', 'height=800,width=600');
-
-    if (win) {
-      return win;
+    constructor(private waxSigningURL: string) {
+        this.openEventSource = this.openEventSource.bind(this);
+        this.onceEvent = this.onceEvent.bind(this);
     }
 
-    throw new Error('Unable to open popup window');
-  }
+    public async openPopup(url: string): Promise<Window> {
+        const win = await window.open(url, 'WaxPopup', 'height=800,width=600');
 
-  public async openEventSource(url: string, message?: any, win?: Window): Promise<Window> {
-    const openedWindow = win ? win : (await this.openPopup(url));
+        if (win) {
+            return win;
+        }
 
-    if (!openedWindow) {
-      throw new Error('Unable to open a popup window');
+        throw new Error('Unable to open popup window');
     }
 
-    if (typeof message === 'undefined') {
-      return openedWindow;
+    public async openEventSource(url: string, message?: any, win?: Window): Promise<Window> {
+        const openedWindow = win ? win : (await this.openPopup(url));
+
+        if (!openedWindow) {
+            throw new Error('Unable to open a popup window');
+        }
+
+        if (typeof message === 'undefined') {
+            return openedWindow;
+        }
+
+        const postTransaction = async (event: any) => {
+            if (event.data.type === 'READY') {
+                openedWindow.postMessage(message, this.waxSigningURL);
+            }
+        };
+
+        const eventPromise = this.onceEvent(openedWindow, this.waxSigningURL, postTransaction, 'READY');
+
+        await Promise.race([eventPromise, this.timeout()]).catch(err => {
+            if (err.message !== 'Timeout') {
+                throw err;
+            }
+
+            openedWindow.postMessage(message, this.waxSigningURL);
+        });
+
+        return openedWindow;
     }
 
-    const postTransaction = async (event: any) => {
-      if (event.data.type === 'READY') {
-        openedWindow.postMessage(message, this.waxSigningURL);
-      }
-    };
+    public async onceEvent<T>(source: Window, origin: string, action: (e: MessageEvent) => T, type?: string): Promise<T> {
+        return new Promise((resolve, reject) => {
+            window.addEventListener(
+                'message',
+                async function onEvent(event) {
+                    if (event.origin !== origin) {
+                        return;
+                    }
 
-    const eventPromise = this.onceEvent(openedWindow, this.waxSigningURL, postTransaction, 'READY');
+                    if (event.source !== source) {
+                        return;
+                    }
 
-    await Promise.race([eventPromise, this.timeout()]).catch(err => {
-      if (err.message !== 'Timeout') {
-        throw err;
-      }
+                    if (typeof event.data !== 'object') {
+                        return;
+                    }
 
-      openedWindow.postMessage(message, this.waxSigningURL);
-    });
+                    if (type && (!event.data.type || event.data.type !== type)) {
+                        return;
+                    }
 
-    return openedWindow;
-  }
+                    try {
+                        resolve(await action(event));
+                    } catch (e) {
+                        reject(e);
+                    }
 
-  public async onceEvent<T>(source: Window, origin: string, action: (e: MessageEvent) => T, type?: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-      window.addEventListener(
-          'message',
-          async function onEvent(event) {
-            if (event.origin !== origin) {
-              return;
-            }
+                    window.removeEventListener('message', onEvent, false);
+                },
+                false
+            );
+        });
+    }
 
-            if (event.source !== source) {
-              return;
-            }
-
-            if (typeof event.data !== 'object') {
-              return;
-            }
-
-            if (type && (!event.data.type || event.data.type !== type)) {
-              return;
-            }
-
-            try {
-              resolve(await action(event));
-            } catch (e) {
-              reject(e);
-            }
-
-            window.removeEventListener('message', onEvent, false);
-          },
-          false
-      );
-    });
-  }
-
-  private timeout = async () => new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), 5000));
+    private timeout = async () => new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), 5000));
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,15 @@
 export interface IWhitelistedContract {
-  contract: string;
-  domain: string;
-  recipients: string[];
+    contract: string;
+    domain: string;
+    recipients: string[];
 }
 
 export interface ISigningResponse {
-  serializedTransaction: Uint8Array;
-  signatures: string[];
+    serializedTransaction: Uint8Array;
+    signatures: string[];
 }
 
 export interface ILoginResponse {
-  account: string;
-  keys: string[];
+    account: string;
+    keys: string[];
 }
